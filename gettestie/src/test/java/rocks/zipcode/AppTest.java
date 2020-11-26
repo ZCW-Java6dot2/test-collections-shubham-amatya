9f84c4024baa33f68f7e5c4882fd734ebac85608
shubham <shubham@zipcodes-MBP-5.fios-router.home>
2020-11-26 12:55:01 -0500

    HashSet and Arraylist tests done

--- gettestie/src/test/java/rocks/zipcode/AppTest.java
+++ gettestie/src/test/java/rocks/zipcode/AppTest.java
@@ -1,18 +1,23 @@
-package rocks.zipcode;
+package rocks.zipcode.rocks.zipcode;
 
 import org.junit.Test;
+import rocks.zipcode.App;
 
 import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest {
+public class AppTest{
+    App app =new App();
+
     /**
      * Rigorous Test.
      */
     @Test
+
     public void testApp() {
-        assertTrue(true);
+
+       assertTrue(true);
     }
 }
---
+++ gettestie/src/test/java/rocks/zipcode/TestHashSet.java
@@ -0,0 +1,20 @@
+package rocks.zipcode.rocks.zipcode;
+
+import org.junit.Test;
+import rocks.zipcode.Person;
+
+
+public class TestHashSet {
+
+    @Test
+    public void hashSetTest(){
+        Person person1 = new Person ()
+
+
+
+
+
+
+    }
+
+}
